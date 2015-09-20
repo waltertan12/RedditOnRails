7cece964c71e0bbd7074f03016cef84713eedf6d
Walter Tan <waltertan12@gmail.com>
2015-09-19 19:50:51 -0700

    Edit comment forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,12 @@ elmo   = User.create!(user_name: "elmo", password: "password")
 grouch = User.create!(user_name: "oscar", password: "password")
 bbird  = User.create!(user_name: "bigbird", password: "password")
 
-elmo.moderated_subs.create!(title: "Weightlifting", description: "Lift weights?")
-grouch.moderated_subs.create!(title: "Trash Cans", description: "My home")
+wl    = elmo.moderated_subs.create!(title: "Weightlifting", 
+                                    description: "Lift weights?")
+trash = grouch.moderated_subs.create!(title: "Trash Cans", 
+                                    description: "My home")
+
+tfw = trash.posts.create(author_id: grouch.id,
+                         title: "tfw living in a trash can",
+                         url: "http://i.imgur.com/B9Wn83f.png",
+                         description: "i live in a trash can")
